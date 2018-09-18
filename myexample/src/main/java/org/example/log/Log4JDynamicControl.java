b78d2a70235444610f8aa3dc0bc1989632fd1d75
CPSDC <CPSDC@DESKTOP-7T1PPNB>
2018-09-18 14:25:08 +0800

    log4j controller

--- myexample/src/main/java/org/example/log/Log4JDynamicControl.java
+++ myexample/src/main/java/org/example/log/Log4JDynamicControl.java
@@ -7,12 +7,14 @@ import org.apache.logging.log4j.web.Log4jWebSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Log4JDynamicControl {
 	@Autowired
 	private ServletContext servletContext;
 
+	@ResponseBody
 	@RequestMapping("/log4j.do")
 	public String log4j() {
 		final LoggerContext ctx  = (LoggerContext)servletContext.getAttribute(Log4jWebSupport.CONTEXT_ATTRIBUTE);
